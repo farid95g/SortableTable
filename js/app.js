b20a927038004893af52a3d18cca0dfb569d6c97
farid95g <f95guluzade@gmail.com>
2020-11-29 14:59:47 +0400

    initial project structure

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const table = document.querySelector("table.table");
+const tableHeader = document.querySelector("table.table thead");
+const tableBody = document.querySelector("table.table tbody");
+
+/** Testing Area */
+console.log(table);
+console.log(tableHeader);
+console.log(tableBody);
