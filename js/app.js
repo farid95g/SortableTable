e49b8a63baee173462cbd4e799c127d3d1281659
farid95g <f95guluzade@gmail.com>
2020-11-29 15:45:30 +0400

    custom table style and jsonplaceholder api data

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
-const table = document.querySelector("table.table");
-const tableHeader = document.querySelector("table.table thead");
-const tableBody = document.querySelector("table.table tbody");
+const table = document.querySelector("table");
+const tableHeader = document.querySelector("table thead");
+const tableBody = document.querySelector("table tbody");
 
 /** Testing Area */
 console.log(table);
 console.log(tableHeader);
 console.log(tableBody);
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then(json => json.filter(todo => todo.id <= 10))
+  .then(
+    json => json.forEach(todo => {
+      const row = `
+        <tr>
+          <td scope="row" class="px-4">${todo.userId}</td>
+          <td class="px-4">${todo.id}</td>
+          <td class="px-4">${todo.title}</td>
+          <td class="px-4">${todo.completed}</td>
+        </tr>
+      `;
+      tableBody.innerHTML += row;
+    })
+  );
