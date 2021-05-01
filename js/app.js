b386c5fa22b3c1c9b152f868a60e570dc57cf442
farid95g <f95guluzade@gmail.com>
2021-05-01 23:37:53 +0400

    sort icons and function for getting data

--- js/app.js
+++ js/app.js
@@ -1,14 +1,28 @@
-const table = document.querySelector("table");
-const tableHeader = document.querySelector("table thead");
 const tableBody = document.querySelector("table tbody");
 const sortBtn = document.querySelectorAll(".sort");
 
-fetch('https://jsonplaceholder.typicode.com/todos')
-  .then(response => response.json())
-  .then(json => json.filter(todo => todo.id <= 10))
-  .then(
-    json => json.forEach(todo => {
-      const row = `
+/**
+ * icons when column will be sorted:
+ * <i class="fas fa-sort-numeric-up-alt"></i>
+ * <i class="fas fa-sort-alpha-up-alt"></i>
+ */
+
+function toDoService(url, cb) {
+  try {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => cb(data));
+  } catch (ex) {
+    console.log(`Failed! ${ex}.`);
+  } finally {
+    console.log("Success!");
+  }
+}
+
+function fillTable(num) {
+  toDoService("https://jsonplaceholder.typicode.com/todos", data => {
+    data.filter(d => d.id <= num).forEach(todo => {
+      let row = `
         <tr>
           <td scope="row" class="px-4">${todo.userId}</td>
           <td class="px-4">${todo.id}</td>
@@ -16,20 +30,9 @@ fetch('https://jsonplaceholder.typicode.com/todos')
           <td class="px-4">${todo.completed}</td>
         </tr>
       `;
-      tableBody.innerHTML += row;
-    })
-  );
-  
-sortBtn.forEach((btn, i) => {
-  btn.addEventListener("click", e => {
-    const rows = document.querySelectorAll("tbody tr");
-    const cells = [];
-    rows.forEach(row => {
-      cells.push(row.children[i].textContent);
-    })
-    const sorted = cells.sort();
-    console.log(sorted)
+      tableBody.insertAdjacentHTML("beforeend", row);
+    });
   });
-})
+};
 
-/** Testing Area Below */
+document.addEventListener("DOMContentLoaded", fillTable(10));
