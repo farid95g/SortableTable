bc6f4597ed33868afcd55cf2012dd73fb2863d00
farid95g <f95guluzade@gmail.com>
2021-05-10 22:38:37 +0400

    separate methods for sorting different data types

--- js/sort.js
+++ js/sort.js
@@ -12,22 +12,33 @@ class Sort {
   }
 
   sortByColumn(col, prop) {
-    console.log(typeof(this.data[0][col]));
     if (typeof(this.data[0][col]) === "number") {
-      this.data = !this.sorted[prop]
-        ? this.data = this.data.sort((u1, u2) => u2[col] - u1[col])
-        : this.data = this.data.sort((u1, u2) => u1[col] - u2[col]);
+      this.sortNum(col, prop);
     } else if (typeof(this.data[0][col]) === "string") {
-      this.data = !this.sorted[prop]
-        ? this.data = this.data.sort((u1, u2) => u2[col].localeCompare(u1[col]))
-        : this.data = this.data.sort((u1, u2) => u1[col].localeCompare(u2[col]));
+      this.sortStr(col, prop);
     } else if (typeof(this.data[0][col]) === "boolean") {
-      this.data = !this.sorted.byTaskCompletion
-        ? this.data = this.data.sort((u1, u2) => Number(u2[col]) - Number(u1[col]))
-        : this.data = this.data.sort((u1, u2) => Number(u1[col]) - Number(u2[col]));
+      this.sortBool(col, prop);
     }
     this.sorted[prop] = !this.sorted[prop];
     return this.data;
+  }
+
+  sortNum(col, prop) {
+    this.data = !this.sorted[prop]
+        ? this.data = this.data.sort((u1, u2) => u2[col] - u1[col])
+        : this.data = this.data.sort((u1, u2) => u1[col] - u2[col]);
+  }
+
+  sortStr(col, prop) {
+    this.data = !this.sorted[prop]
+        ? this.data = this.data.sort((u1, u2) => u2[col].localeCompare(u1[col]))
+        : this.data = this.data.sort((u1, u2) => u1[col].localeCompare(u2[col]));
+  }
+
+  sortBool(col, prop) {
+    this.data = !this.sorted.byTaskCompletion
+        ? this.data = this.data.sort((u1, u2) => Number(u2[col]) - Number(u1[col]))
+        : this.data = this.data.sort((u1, u2) => Number(u1[col]) - Number(u2[col]));
   }
 
   /** function for sorting according to user id */
